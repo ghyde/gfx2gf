8e35fc6646cf440ffa4d4d045fa2406ce1d226e7
Garrett Hyde <github@garretthyde.com>
2014-09-12 01:12:08 -0600

    Fix bug when using the '-o' option and creating nested folders.

--- gfx2gf.sh
+++ gfx2gf.sh
@@ -2,7 +2,7 @@
 
 ##########################################################################
 #                                                                        #
-# gfx2gf.sh v1.0 (20140911)                                              #
+# gfx2gf.sh v1.0.1 (20140912)                                            #
 # by Garrett Hyde                                                        #
 #                                                                        #
 # This bash script is based upon Jeremy Williams Window's batch file.    #
@@ -13,23 +13,25 @@
 
 USAGE="$0 [-o $(tput smul)directory$(tput rmul)] $(tput smul)file$(tput rmul)..."
 
-#Check if ImageMagick is installed
+# Check if ImageMagick is installed
 if ! type convert > /dev/null; then
     echo "You need to install ImageMagick first!"
     exit 1
 fi
 
-#Check if Ghostscript is installed
+# Check if Ghostscript is installed
 if ! type gs > /dev/null; then
     echo "You need to install Ghostscript first! (Required in order to run \`convert\`)"
     exit 1
 fi
 
+# Check usage
 if [ "$#" -lt 1 ]; then
     echo $USAGE
     exit 1
 fi
 
+# Parse options
 base_output=""
 while getopts ":o:" opt; do
     case $opt in
@@ -49,7 +51,9 @@ while getopts ":o:" opt; do
 done
 shift $((OPTIND - 1))
 
+# Process graphic files
 for input_path in "$@"; do
+
     # Establish output directory
     input_file="$(basename "$input_path")"
     output_dir="$(basename "$input_path" .${input_path##*.})"
@@ -59,10 +63,10 @@ for input_path in "$@"; do
         output_path="$(dirname "$input_path")/$output_dir"
     fi
 
-    echo "$output_path"
     # Make output directory
     if [ -e "$output_path" ]; then
         echo "Cannot create output directory \"$output_path\" because it already exists."
+        #TODO print error message and continue to next file instead of exiting
         exit 1
     else
         mkdir -p "$output_path"
@@ -70,11 +74,11 @@ for input_path in "$@"; do
 
     # Grab information about source file (height, width, delay)
     file_info=$(identify -format "%h %w %T" "$input_path"[0])
-    height=$(echo $file_info | awk '{print $1}')
-    width=$(echo $file_info | awk '{print $2}')
-    delay=$(echo $file_info | awk '{print $3}')
+    height=$(echo "$file_info" | awk '{print $1}')
+    width=$(echo "$file_info" | awk '{print $2}')
+    delay=$(echo "$file_info" | awk '{print $3}')
 
-    # Convert to BMP and extract animated GIF into separate files
+    # Convert to graphic into BMP files
     echo "Converting \"$input_file\" to Game Frame format..."
     echo "-----"
     ffmpeg -an -r 1 -i "$input_path" -r 1 -f image2 -pix_fmt bgr24 -s 16x16 "$output_path/frame_%06d.bmp"
@@ -88,6 +92,7 @@ for input_path in "$@"; do
         echo "File has more than 196000 frames. This might not work."
     fi
 
+    # Check if need to use nested folders
     if [ "$frames" -gt 1999 ]; then
         nested=1
     else
@@ -98,6 +103,8 @@ for input_path in "$@"; do
     echo "Creating filmstrip..."
     montage "$output_path/frame_*.bmp" -mode concatenate -tile 1x -type truecolor "$output_path/0.bmp"
     if [ "$frames" -gt 1 ]; then
+        # Create multiframe animation
+
         echo "Filmstrip conversion finished!"
 
         if [ "$delay" -lt 4 ]; then
@@ -147,6 +154,7 @@ panoff = false
 
         echo "File \"config.ini\" written!"
 
+        # Generate preview GIF
         echo "Generating preview..."
         delay=$(($delay / 10))
         if [ "$frames" -le 250 ]; then
@@ -158,6 +166,8 @@ panoff = false
         echo "Preview created!"
 
     else
+        # Create single frame animation
+
         echo "Graphic conversion finished!"
         echo "Generating preview..."
         convert -filter box -resize 128x128 "$output_path/frame_*.bmp" "$output_path"/"$output_dir"_preview.gif
@@ -167,27 +177,31 @@ panoff = false
     # Cleanup
     find "$output_path" -name 'frame_*.bmp' -delete
 
-    # Build nest
+    # Build nested folders
     if [ "$nested" -eq 1 ]; then
         echo "Long video detected; nesting folders..."
 
+        # Generate filmstrips
         convert -crop 16x32000x0x0 "$output_path/0.bmp" "$output_path/strip_%01d.bmp"
         strips=$(ls "$output_path" | grep "strip_" | wc -l)
         echo "$strips filmstrips created."
 
+        # Create nested folders and move filmstrips into those folders
+        echo "Creating nested folders"
         for i in $(seq 0 $(($strips - 1))); do
-            echo "Making nested folder $i"
-            mkdir "$output_dir/$i"
-            mv "$output_dir/strip_$i.bmp" "$output_dir/$i/0.bmp"
-            cp "$output_dir/config.ini" "$output_dir/$i/"
+            mkdir "$output_path/$i"
+            mv "$output_path/strip_$i.bmp" "$output_path/$i/0.bmp"
+            cp "$output_path/config.ini" "$output_path/$i/"
         done
-        rm -f "$output_dir/config.ini"
+
+        # Cleanup
+        rm -f "$output_path/config.ini"
         rm -f "$output_path/0.bmp"
-        echo "Nesting folders complete!"
+
+        echo "Nested folders created!"
 
     fi
     
     echo "Process finished!"
 done
-
 exit 0
